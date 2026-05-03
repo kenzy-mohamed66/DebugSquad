bfe8e1a6d3195e1deb303a5b4b53efc84767d01d
kenzy-mohamed66 <kenzyshalaby2006@gmail.com>
2026-05-04 02:07:22 +0300

    kk

--- Auth/User.java
+++ Auth/User.java
@@ -7,17 +7,17 @@ public class User implements Serializable {
 
     private static int idCounter = 1;
 
-    private int           userID;
-    private String        fullName;
-    private String        password;
-    private String        email;
+    private int userID;
+    private String fullName;
+    private String password;
+    private String email;
     private LocalDateTime createdAt;
 
     public User(String fullName, String email, String password) {
-        this.userID    = idCounter++;
-        this.fullName  = fullName;
-        this.email     = email;
-        this.password  = password;
+        this.userID = idCounter++;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
         this.createdAt = LocalDateTime.now();
     }
 
@@ -33,14 +33,12 @@ public class User implements Serializable {
                 && this.password.equals(inputPassword);
     }
 
-    public void logout() {
-        // implemented in LOGIN UI by clearing currentUser reference
-    }
+    public void logout() {}
 
-    public int           getUserID()    { return userID; }
-    public String        getFullName()  { return fullName; }
-    public String        getEmail()     { return email; }
-    public String        getPassword()  { return password; }
+    public int getUserID()          { return userID; }
+    public String getFullName()     { return fullName; }
+    public String getEmail()        { return email; }
+    public String getPassword()     { return password; }
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setFullName(String n)   { this.fullName = n; }
     public void setEmail(String e)      { this.email = e; }
